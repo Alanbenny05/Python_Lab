13ba19c10c70902a406c4b6e679bc5c82a6b06b7
Alan Benny <alanbennynedumkattil05@gmail.com>
2025-09-17 15:49:07 +0530

    Add files via upload

---
+++ positive_list.py
@@ -0,0 +1,3 @@
+nums=list(map(int,input("Enter integers separated by space:").split()))
+positive=[n for n in nums if n>0]
+print("Positive Numbers:",positive)
---
+++ square_of_n.py
@@ -0,0 +1,3 @@
+N=int(input("Enter the number: "))
+squares=[n**2 for n in range(1,N+1)]
+print("Square Numbers:",squares)
